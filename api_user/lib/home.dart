e1a7180083fa2072f43ede787816f0a74322e891
Gokul-Gokzzz <gokulunniu88@gmail.com>
2024-01-25 22:17:03 +0530

    first commit

---
+++ api_user/lib/home.dart
@@ -0,0 +1,91 @@
+import 'dart:convert';
+
+import 'package:api_user/model.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<UserDetailes> userDetails = [];
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: getData(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 15, horizontal: 10),
+                itemCount: userDetails.length,
+                itemBuilder: (context, index) {
+                  return Container(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 10, horizontal: 15),
+                    margin: const EdgeInsets.all(10),
+                    height: 220,
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        getText(index, 'ID', userDetails[index].id.toString()),
+                        getText(
+                            index, 'Name', userDetails[index].name.toString()),
+                        getText(index, 'Email',
+                            userDetails[index].email.toString()),
+                        getText(index, 'Phone',
+                            userDetails[index].phone.toString()),
+                        getText(index, 'Website',
+                            userDetails[index].website.toString()),
+                        getText(index, 'Company Name',
+                            userDetails[index].company.toString()),
+                        getText(index, 'Address',
+                            ' - ${userDetails[index].address.suite.toString()}, ${userDetails[index].address.street.toString()}, ${userDetails[index].address.city.toString()} - ${userDetails[index].address.zipcode.toString()}')
+                      ],
+                    ),
+                  );
+                });
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        });
+  }
+
+  Text getText(int index, String fieldName, String content) {
+    return Text.rich(TextSpan(children: [
+      TextSpan(
+          text: fieldName,
+          style: const TextStyle(
+              color: Colors.white, fontSize: 16, fontWeight: FontWeight.bold)),
+      TextSpan(
+          text: content,
+          style: const TextStyle(
+              color: Colors.white, fontSize: 16, fontWeight: FontWeight.bold))
+    ]));
+  }
+
+  Future<List<UserDetailes>> getData() async {
+    final response =
+        await http.get(Uri.parse('https://jsonplaceholder.typicode.com/users'));
+    var data = jsonDecode(response.body.toString());
+    if (response.statusCode == 200) {
+      for (Map<String, dynamic> index in data) {
+        userDetails.add(UserDetailes.fromJson(index));
+      }
+      return userDetails;
+    } else {
+      return userDetails;
+    }
+  }
+}
---
+++ api_user/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:api_user/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+          title: Text('API USER'),
+          centerTitle: true,
+        ),
+        body: HomePage(),
+      ),
+    );
+  }
+}
